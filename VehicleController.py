f025a41a17159ad5d8167f8610696ededa65cf5a
Iban Leginyora <ibanleginyora@gmail.com>
2025-07-15 09:47:12 +0200

    Add : VehicleController.py

---
+++ VehicleController.py
@@ -0,0 +1,19 @@
+from State_classes import *
+
+class VehicleController:
+    def __init__(self):
+        self.state = OFF(self)
+
+    def change_state(self, new_state_class):
+        #if hasattr(self.state, "__del__"):
+        #    self.state.__del__()
+        del self.state
+        gc.collect()
+        self.state = new_state_class(self)
+
+    def handle_event(self, event_name, *args):
+        method = getattr(self.state, event_name, None)
+        if method:
+            method(*args)
+        else:
+            print(f"L'événement '{event_name}' n'est pas disponible dans {self.state.__class__.__name__}")
