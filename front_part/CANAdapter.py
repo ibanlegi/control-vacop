ad0a4a5d6441db1e5724b71f673dc0655b42b05d
Iban Leginyora <ibanleginyora@gmail.com>
2025-07-18 10:00:58 +0200

    Add devices classes

---
+++ front_part/CANAdapter.py
@@ -0,0 +1,28 @@
+from CAN_system.CANSystem import CANSystem
+import time
+
+class CANAdapter:
+    def __init__(self, channel='can0', interface='socketcan', device_name='BRAKE', verbose=False):
+        self.verbose = verbose
+        self.canSystem = CANSystem(device_name=device_name, channel=channel, interface=interface, verbose=verbose)
+        self.running = True
+
+    def send(self, device_id, order_id, data=None):
+        self._print(f"[CANAdapter] Sending {device_id=} {order_id=} {data=}")
+        self.canSystem.can_send(device_id, order_id, data)
+
+    def receive(self):
+        # Returns the next CAN message as a tuple or None if no new message
+        msg = self.canSystem.listener.can_input()
+        if msg and isinstance(msg, tuple) and len(msg) == 3:
+            self._print(f"[CANAdapter] Received message: {msg}")
+            return msg
+        return None
+
+    def stop(self):
+        self.running = False
+        self.canSystem.stop()
+
+    def _print(self, *args, **kwargs):
+        if self.verbose:
+            print("[CANAdapter]", *args, **kwargs)
