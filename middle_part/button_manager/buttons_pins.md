c30f7cb31f6d3978caf049fb5b146dae35f81c69
Iban Leginyora <ibanleginyora@gmail.com>
2025-07-21 21:52:02 +0200

    Configure button manager with abstract controller

--- middle_part/button_manager/buttons_pins.md
+++
@@ -1,4 +0,0 @@
-PARK        : PIN 22        : BUTTON (0-->1)
-AUTO / MANU : PIN 23        : SWITCH (0 | 1)
-ON / OFF    : PIN 24        : SWITCH (0 | 1)
-REVERSE     : NO CONNECTED
--- middle_part/button_part/ParkPin.py
+++ middle_part/button_part/ParkPin.py
@@ -1,15 +1,17 @@
+# A tester 
+
 import RPi.GPIO as GPIO
 import time
-from CAN_system.CANSystem import CANSystem
+from ...CAN_system.CANSystem import CANSystem
 
 
 class ParkPin:
-    def __init__(self, park_pin = 22):
+    def __init__(self, parkPin):
         GPIO.setmode(GPIO.BCM)
-        self.pin = park_pin
+        self.pin = parkPin
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-        self.can_system = CANSystem(device_name="STEER", verbose=True)
-        self.previous_state = GPIO.input(self.pin) # we consider that the button is not pressed when initialized
+        self.canSystem = CANSystem(device_name="STEER", verbose=True)
+        self.previousState = GPIO.input(self.pin) # we consider that the button is not pressed when initialized
         GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=self._on_state_change, bouncetime=100)
     
     def _print(self, *args, **kwargs):
@@ -17,11 +19,11 @@ class ParkPin:
             print(*args, **kwargs)
 
     def _on_state_change(self):
-        button_state = GPIO.input(self.pin)
-        if button_state != self.previous_state:
-            self.previous_state = button_state
-            self._print(f"Parking button {'pressed' if button_state else 'released'}, button = {button_state}")
-            self.can_system.send("OBU", "bouton_park", button_state)
+        buttonState = GPIO.input(self.pin)
+        if buttonState != self.previousState:
+            self.previousState = buttonState
+            self._print(f"Parking button {'pressed' if buttonState else 'released'}, button = {buttonState}")
+            self.canSystem.send("OBU", "bouton_park", buttonState)
     
     def cleanup(self):
         GPIO.remove_event_detect(self.pin)
@@ -37,5 +39,5 @@ class ParkPin:
             GPIO.cleanup()
 
 if __name__ == "__main__":
-    park_pin = ParkPin()
-    park_pin.run()
+    parkPin = ParkPin(22)
+    parkPin.run()
